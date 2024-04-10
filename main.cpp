c089aae7b2f786b9aaf8c2103703b6925fac5b93
Peter Morganelli <peter.morganelli@tufts.edu>
2024-04-10 19:04:01 -0400

    added main.cpp

---
+++ main.cpp
@@ -0,0 +1,26 @@
+/*
+ *  main.cpp
+ *
+ *  Peter Morganelli pmorga01 and Brendan Roy broy01
+ *
+ *  CS15 proj_gerp
+ *
+ *  main.cpp file for gerp
+ */
+
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+
+/*
+* name:      main
+* purpose:   the main function to run all the metrosim elements
+* arguments: the number of command line arguments and the array of commands
+* returns:   0 if nothing went wrong
+* effects:   runs all the functions involved for metrosim
+*/
+int main(int argc, char *argv[]){
+
+}
